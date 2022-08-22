9a216e45a618d7ab6eac8885294906266d24bd66
izazahmed0177 <izazahmed0177@gmail.com>
2022-08-22 10:23:45 +0600

    manager field  add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 // 
 document.getElementById('btn-calculate').addEventListener('click',function(){
     console.log('click')
-    const playerCost=getInputFieldValueById('player-cost')
+    const playerCost=getInputFieldValueById('player-cost');
     console.log(playerCost)
+})
+//
+
+//-------
+
+document.getElementById('btn-calculate-total').addEventListener('click',function(){
+
+const mangerCost=getInputFieldValueById('manger-cost');
+const coachCost=getInputFieldValueById('coach-cost');
+
+console.log(mangerCost,coachCost)
+
 })
