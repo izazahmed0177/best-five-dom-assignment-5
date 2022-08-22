04052df9634b90acc504f6f98bb3ee684dd9dc5d
izazahmed0177 <izazahmed0177@gmail.com>
2022-08-22 14:58:45 +0600

    player loop use and get

--- js/player.js
+++ js/player.js
@@ -1,5 +1,40 @@
+//
+
+const players = [];
+
+
+function  displayProduct() {
+    let totalPlayer = 0;
+
+
+    const playerList=document.getElementById('player-list')
+    playerList.textContent='';
+
+    for (let i = 0; i < players.length; i++) {
+        const element = players[i];
+        console.log(element);
+        
+        
+    }
+  
+    
+}
+
+//---------------
+
 function addToPlayer(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
+
     console.log(playerName)
+
+    players.push(playerName);
+
+  
+    console.log(players);
+
     
+    let totalPlayersList=players.length;
+    console.log(totalPlayersList);
+
+    displayPlayer();
 }
