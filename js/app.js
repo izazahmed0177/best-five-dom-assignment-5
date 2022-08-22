a312df9fbcb3f852bfee79c03b03a1e69f4375d5
izazahmed0177 <izazahmed0177@gmail.com>
2022-08-22 16:54:40 +0600

    tabile add value

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // 
+const players = [];
+//
 document.getElementById('btn-calculate').addEventListener('click',function(){
     // console.log('click')
     const playerCost=getInputFieldValueById('player-cost');
@@ -27,3 +29,56 @@ const calculateTotalAmount = playerExpenses + mangerCost + coachCost;
 
 setTextElementValueById('calculate-total-amount',calculateTotalAmount);
 })
+
+
+
+//------------------------------------------------------
+function displayPlayer() {
+
+    const playerList1=document.getElementById('player-list');
+        playerList1.textContent = '';
+    
+        for (let i = 0; i < players.length; i++) {
+            const element = players[i];
+            console.log(players[i]);
+    
+            const tr = document.createElement("tr");
+    
+            tr.innerHTML = ` 
+            <th>${i+1}</th>
+            <td>${players[i].playerName}</td>
+             `;
+            playerList1.appendChild(tr);    
+        }
+      
+    
+}
+
+
+
+
+//----------------------------------
+
+function addToPlayer(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+
+    // console.log(playerName)
+
+       const playerObj={
+            playerName:playerName,
+         };
+    players.push(playerObj);
+
+  
+    // console.log(players);
+    console.log(playerObj);
+
+    
+    let totalPlayersList=players.length;
+    console.log(totalPlayersList);
+
+    displayPlayer();
+
+}
+
+
--- js/player.js
+++ js/player.js
@@ -1,40 +1,104 @@
 //
 
-const players = [];
+// const players = [];
 
 
-function  displayProduct() {
-    let totalPlayer = 0;
-
-
-    const playerList=document.getElementById('player-list')
-    playerList.textContent='';
-
-    for (let i = 0; i < players.length; i++) {
-        const element = players[i];
-        console.log(element);
-        
-        
-    }
-  
+// function displayPlayer() {
     
-}
+
+
+//     const playerList=document.getElementById('player-list');
+//     playerList.textContent = '';
+
+//     for (let i = 0; i < players.length; i++) {
+//         const element = players[i];
+//         console.log('ooioii',players[i]);
+
+//         const tr = document.createElement("tr");
+
+//         tr.innerHTML = ` 
+//         <th>${i+1}</th>
+//         <td>${players[i].playerName}</td>
+//          `;
+//         playerList.appendChild(tr);    
+//     }
+  
+//     console.log('hhggffdf')
+    
+// }
+
 
 //---------------
 
-function addToPlayer(element) {
-    const playerName = element.parentNode.parentNode.children[0].innerText;
+// function addToPlayer(element) {
+//     const playerName = element.parentNode.parentNode.children[0].innerText;
 
-    console.log(playerName)
+//     // console.log(playerName)
 
-    players.push(playerName);
+//        const playerObj={
+//             playerName:playerName,
+//          }
+//     players.push(playerObj);
 
   
-    console.log(players);
+//     // console.log(players);
+//     console.log(playerObj);
 
     
-    let totalPlayersList=players.length;
-    console.log(totalPlayersList);
+//     let totalPlayersList=players.length;
+//     console.log(totalPlayersList);
 
-    displayPlayer();
-}
+//     // displayPlayer();
+
+
+
+
+
+//     //------------------
+
+
+//     const playerList=document.getElementById('player-list');
+//     playerList.textContent = '';
+
+//     for (let i = 0; i < players.length; i++) {
+//         const element = players[i];
+//         console.log('ooioii',players[i]);
+
+//         const tr = document.createElement("tr");
+
+//         tr.innerHTML = ` 
+//         <th>${i+1}</th>
+//         <td>${players[i].playerName}</td>
+//          `;
+//         playerList.appendChild(tr);    
+//     }
+  
+//     console.log('hhggffdf')
+// }
+
+//----------------------
+
+
+// function displayPlayer() {
+    
+
+
+//     const playerList=document.getElementById('player-list');
+//     playerList.textContent = '';
+
+//     for (let i = 0; i < players.length; i++) {
+//         const element = players[i];
+//         console.log('ooioii',players[i]);
+
+//         const tr = document.createElement("tr");
+
+//         tr.innerHTML = ` 
+//         <th>${i+1}</th>
+//         <td>${players[i].playerName}</td>
+//          `;
+//         playerList.appendChild(tr);    
+//     }
+  
+//     console.log('hhggffdf')
+    
+// }
